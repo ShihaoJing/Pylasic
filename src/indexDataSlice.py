d72a02eaa1becda2ed25243e36b602f2855a8a8a
Spike Wang <cupman000@gmail.com>
2017-04-18 03:39:42 -0400

    traverse all files in data folder

--- src/indexDataSlice.py
+++ src/indexDataSlice.py
@@ -20,30 +20,30 @@ def slice_list(input, size):
 
 #dataFolder = '../data/College Scorecard/'
 dataFolder = '../data/Consolidated State Performance Report, 2009-10'
-
 # each dataset folder has a meta.json file, which contains some meta data
 metaJSON = json.load(open(os.path.join(dataFolder, 'meta.json')))
-for filename in os.listdir(dataFolder):
-    if filename.endswith('.csv'):
-        filepath =  os.path.join(dataFolder, filename)
-        print('indexing: ' + filepath)
-        # read csv file
-        dataset = csv.DictReader(open(filepath, 'r'))
-        # convert csv file to list of dictionaries
-        dataList = list(dataset)
-        # list of all attributes (column names)
-        attrList = list(dataList[0].keys())
-        # cut into 30 slices
-        dataslices = slice_list(dataList, 30)
-        for datagroup in dataslices:
-            data = {'data': datagroup}
-            data['datasetName'] = metaJSON['title']
-            data['datasetDescription'] = metaJSON['description']
-            data['datasetDistribution'] = metaJSON['distribution']
-            data['keyword'] = metaJSON['keyword']
-            data['filename'] = filename
-            data['attrList'] = attrList
-            # index name can be the name of dataset
-            # doc_type name doesn't matter.
-            res = es.index(index="testIndex", doc_type='basetype', body=data)
-            print(res['created'])
+for dirpath, dirnames, filenames in os.walk(dataFolder):
+    for filename in filesnames:
+        if filename.endswith('.csv'):
+            filepath =  os.path.join(dataFolder, filename)
+            print('indexing: ' + filepath)
+            # read csv file
+            dataset = csv.DictReader(open(filepath, 'r'))
+            # convert csv file to list of dictionaries
+            dataList = list(dataset)
+            # list of all attributes (column names)
+            attrList = list(dataList[0].keys())
+            # cut into 30 slices
+            dataslices = slice_list(dataList, 30)
+            for datagroup in dataslices:
+                data = {'data': datagroup}
+                data['datasetName'] = metaJSON['title']
+                data['datasetDescription'] = metaJSON['description']
+                data['datasetDistribution'] = metaJSON['distribution']
+                data['keyword'] = metaJSON['keyword']
+                data['filename'] = filename
+                data['attrList'] = attrList
+                # index name can be the name of dataset
+                # doc_type name doesn't matter.
+                res = es.index(index="testIndex", doc_type='basetype', body=data)
+                print(res['created'])
