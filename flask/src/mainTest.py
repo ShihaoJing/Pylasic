ce368572bd4a5954a03694fd41bfb6dc82c02593
ameliagalgon <amg617@lehigh.edu>
2017-05-08 22:39:33 -0400

    added check functions to clean up code

--- flask/src/mainTest.py
+++ flask/src/mainTest.py
@@ -12,23 +12,23 @@ from nltk.draw.cfg import CFGEditor
   
 
 helpMessage = "The \'@\' symbol starts each field search, and is followed by the field you want to search in \nThe \'#\' symbol starts the list of values that you expect to find in the field\nIf you expect a numeric field, the the upper and lower bound of values must be specified.\n To specify the bounds, the following conditions can be used\n a) gte - greater than or equal to\n b) lte - less than or equal to\n c) gt - greater than\n d) lt - less than\nLastly, sperate successive terms inside the \'[..]\' by a comma"
-
+queryOperators = [] #this is the store the operators used in the query to test if it defines the proper boundaries
+    #Check to see if the operator is valid
 #fucntion that parses the query
 def parseQuery(query):
     searchType = None
-    keywords = ["lte","gte","lt","gt"]
     #first check the number of @ symbols in the query
     fields = query.split("@")
     #print(fields)
     bool_count = 0;
     range_count = 0;
-    queryOperators = [] #this is the store the operators used in the query to test if it defines the proper boundaries
     if len(fields) > 1:
         #type of search can either be multi_range or mixed
         for field in fields:
-            input = field.split("#")
-            fieldname = input[0]
-            arguments = input[1].replace("[","").replace("]","")
+            inputValues = field.split("#")
+            print(inputValues)
+            fieldname = inputValues[0]
+            arguments = inputValues[1].replace("[","").replace("]","")
             args = arguments.strip(" ").split(",")
             #print(args)
             for arg in args:
@@ -45,37 +45,14 @@ def parseQuery(query):
                     #check to see that there are two values for the range search
                     if len(args) != 2:
                         printError("A range query should have 2 arguments to set the bounds")
-                    #Check to see if the operator is valid
-                    valid = False
-                    print(argfields[0])
-                
-                    for op in keywords:
-                        if op == argfields[0]:
-                            valid = True
-                            queryOperators.append(op)
-                            break
-                    if not(valid):
-                        printError("Unknown comparison operator")
-                    #the second value should be a numeric value
-                    if not(argfields[1].isdigit()):
-                        printError("Range values must be numeric")
+                    checkProperRange(argfields)
                     range_count = range_count + 1 #this is not the case. Test to see if the name is the argument is valid
                     #TODO: Check to see if the arguments are valid
                 else:
                     #this is an error
                      printError("This is improper syntax")
             #check the operators used in the query to make sure that the bounds of the range search are properly defined
-            if len(queryOperators) != 0: 
-                if len(queryOperators) != 2: #shouldn't need this, but just to double check
-                    printError("A range query should have 2 arguments to set the bounds") 
-                if queryOperators[0] == queryOperators[1]:
-                    printError("Cannot define the same bound twice")
-                if queryOperators[0] == "lt" or queryOperators[0] == "lte":
-                    if queryOperators[1] != "gte" and queryOperators[1] != "gt":
-                        printError("Bounds of the range search are not properly defined")
-                elif queryOperators[0] == "gt" or queryOperators[0] == "gte":
-                    if queryOperators[1] != "lte" and queryOperators[1] != "lt":
-                        printError("Bounds of the range search are not properly defined")
+            checkProperOps()
         if(bool_count != 0 and range_count != 0):
             searchType = "mixed"
         elif(bool_count != 0 and range_count == 0):
@@ -99,34 +76,12 @@ def parseQuery(query):
                 if len(args) != 2:
                     printError("A range query should have 2 arguments to set the bounds")
                 #Check to see if the operator is valid
-                valid = False
-                print(argfields[0])
-                
-                for op in keywords:
-                    if op == argfields[0]:
-                        valid = True
-                        queryOperators.append(op)
-                        break
-                if not(valid):
-                    printError("Unknown comparison operator")
-                #the second value should be a numeric value
-                if not(argfields[1].isdigit()):
-                    printError("Range values must be numeric")
+                checkProperRange(argfields)
                 range_count = range_count + 1
             else:
                 printError("This is improper syntax")
         #check the operators used in the query to make sure that the bounds of the range search are properly defined
-        if len(queryOperators) != 0:
-            if len(queryOperators) != 2: #shouldn't need this, but just to double check
-                printError("A range query should have 2 arguments to set the bounds") 
-            if queryOperators[0] == queryOperators[1]:
-                printError("Cannot define the same bound twice")
-            if queryOperators[0] == "lt" or queryOperators[0] == "lte":
-                if queryOperators[1] != "gte" and queryOperators[1] != "gt":
-                    printError("Bounds of the range search are not properly defined")
-            elif queryOperators[0] == "gt" or queryOperators[0] == "gte":
-                if queryOperators[1] != "lte" and queryOperators[1] != "lt":
-                    printError("Bounds of the range search are not properly defined")
+        checkProperOps()
         #determine the search type
         if bool_count != 0 and range_count != 0:
             printError("Field search cannot contain a range and a value")
@@ -155,11 +110,39 @@ def printError(message):
     print(helpMessage)
     quit()
 
+def checkProperRange(argfields):
+    keywords = ["lte","gte","lt","gt"]
+    valid = False
+    #print(argfields[0])   
+    for op in keywords:
+        if op == argfields[0]:
+            valid = True
+            queryOperators.append(op)
+            break
+    if not(valid):
+        printError("Unknown comparison operator")
+    #the second value should be a numeric value
+    if not(argfields[1].isdigit()):
+        printError("Range values must be numeric")
+
+
+def checkProperOps():
+    if len(queryOperators) != 0:
+        if len(queryOperators) != 2: #shouldn't need this, but just to double check
+            printError("A range query should have 2 arguments to set the bounds") 
+        if queryOperators[0] == queryOperators[1]:
+            printError("Cannot define the same bound twice")
+        if queryOperators[0] == "lt" or queryOperators[0] == "lte":
+            if queryOperators[1] != "gte" and queryOperators[1] != "gt":
+                printError("Bounds of the range search are not properly defined")
+        elif queryOperators[0] == "gt" or queryOperators[0] == "gte":
+            if queryOperators[1] != "lte" and queryOperators[1] != "lt":
+                printError("Bounds of the range search are not properly defined")
 #results = searcher.execute_pylastic_search("University of Arizona", type = 'bool')
 
 # results = searcher.execute_pylastic_search("data/STNAM/ARIZONA", type = 'bool')
 results = []
-query = "@SAT_AVG#[gt:1000, lte:1200] @City#[New York, San Francisco]"
+query = "@SAT_AVG#[gte:1000, lte:1200] @City#[New York]"
 
 query = query.strip(" ")
 if(query[0] == '@'):
