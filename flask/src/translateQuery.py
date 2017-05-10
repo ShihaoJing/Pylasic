dab71f562147c98046e5e4f0010bbed75bba0554
ameliagalgon <amg617@lehigh.edu>
2017-05-09 22:09:18 -0400

    transform query based on dictionary

--- flask/src/translateQuery.py
+++ flask/src/translateQuery.py
@@ -4,8 +4,10 @@ def extractFieldnames(query):
 	fieldnames = []
 	values = query.split("@")[1:]
 	for value in values:
-		fieldname = value.split("#")[0]
-		fieldnames.append(fieldname)
+		#print(value)
+		fields = value.split("#")
+		fieldnames.append([fields[0],fields[1]])
+	#print(fieldnames)
 	return fieldnames
 
 #read in the dictionary
@@ -23,11 +25,15 @@ for field in fieldnames:
 	for i, row in dictionary.iterrows():
 		#print(i)
 		#print friendlyNames[i]
-		if field in str(friendlyNames[i]):
+		if field[0] in str(friendlyNames[i]):
 			if not variableNames[i] in potential_fields:
 				j = j + 1
-				potential_fields.append(variableNames[i])
+				potential_fields.append([variableNames[i],field[1]])
 			if j==5: #adding a limit
 				break
 			#print(potential_fields)
-print(potential_fields)
+#print(potential_fields)
+for field in potential_fields:
+	query += "@%s#%s"%(field[0],field[1])
+
+print query
